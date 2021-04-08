63ebd05ae4d9cb0cf616adf643aa39d87861d5fd
matteo-gamba <matteo.gamba@uzh.ch>
2021-04-08 19:54:09 +0200

    expo trying to run

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Working!</Text>
+      <Text>Hello World</Text>
+      <Button title="Learn More"
+              color="#841584"
+      />
       <StatusBar style="auto" />
     </View>
   );
