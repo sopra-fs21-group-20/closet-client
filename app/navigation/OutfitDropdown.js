0f6da5ea25f5138420396800e4dc300f01b259ef
joedoublev98 <71336966+joedoublev98@users.noreply.github.com>
2021-05-24 21:43:40 +0200

    First part of #79

--- app/navigation/OutfitDropdown.js
+++ app/navigation/OutfitDropdown.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {View, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Platform} from "react-native";
 import {MaterialCommunityIcons} from "@expo/vector-icons";
 import {useRoute} from '@react-navigation/native';
@@ -6,6 +6,7 @@ import {useRoute} from '@react-navigation/native';
 import colors from "../config/colors";
 import routes from "./routes";
 import Text from "../components/Text";
+import {Tooltip} from "react-native-elements";
 
 function OutfitDropdown({navigation, setIsOpen, isOpen}) {
     const route = useRoute();
@@ -21,20 +22,36 @@ function OutfitDropdown({navigation, setIsOpen, isOpen}) {
         console.log(isOpen);
     }, [isOpen]);
 
+    const tooltipRef = useRef(null);
+
+    useEffect(() => {
+        tooltipRef.current.toggleTooltip();
+    }, []);
+
+    const toggleNav = () => {
+        if (route.name !== routes.CREATEOUTFIT) {
+            setTimeout(() => {
+                setIsOpen(!isOpen);
+            }, 0);
+        }
+    }
+
+    const [hasBeenTooltip, setHasBeenTooltip] = useState(false);
+
 
     return (
         <>
             <TouchableOpacity
                 onPress={() => {
-                    if (route.name !== routes.CREATEOUTFIT) {
-                        setIsOpen(!isOpen);
-                    }
+                    toggleNav();
                 }}
                 style={[styles.container, Platform.OS === "android" && isOpen ? {top: 0} : null]}
             >
                 <View style={[styles.title, route.name !== routes.CREATEOUTFIT ? {paddingLeft: 0} : null]}>
-                    <Text style={styles.text}>{route.name === routes.CREATEOUTFIT ? "New Outfit" : route.name}</Text>
+                    <Text
+                        style={styles.text}>{route.name === routes.CREATEOUTFIT ? "New Outfit" : route.name}</Text>
                     {route.name !== routes.CREATEOUTFIT && <View style={styles.iconWrap}>
+
                         <MaterialCommunityIcons
                             name={isOpen ? 'chevron-up' : 'chevron-down'}
                             color={'rgba(255, 255, 255, 0.7)'}
@@ -42,6 +59,21 @@ function OutfitDropdown({navigation, setIsOpen, isOpen}) {
                         />
                     </View>}
                 </View>
+                <Tooltip
+                    popover={<Text>Press here to get to the mirror where you can compose your outfits!</Text>}
+                    ref={tooltipRef}
+                    overlayColor='rgba(0,0,0,0.85)'
+                    width={250}
+                    height={120}
+                    toggleAction='onLongPress'
+                    /*onClose={() => {
+                        toggleNav();
+                    }}*/
+                >
+                    <View style={styles.tooltipped}>
+                        <Text></Text>
+                    </View>
+                </Tooltip>
                 <View style={[styles.dropDown, Platform.OS === "ios" ? {
                     position: "absolute",
                     top: "150%",
@@ -73,6 +105,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         paddingLeft: 31,
+        zIndex: 2,
     },
     dropDown: {
         zIndex: 100,
@@ -88,7 +121,14 @@ const styles = StyleSheet.create({
     iconWrap: {
         marginTop: 2,
         marginLeft: 3
-    }
+    },
+    tooltipped: {
+        position: "absolute",
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'red',
+        zIndex: 1,
+    },
 });
 
 export default OutfitDropdown;
